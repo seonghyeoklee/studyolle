1f5eccbab145b5a0b854d5b798b4788ea630143f
seonghyeoklee <dltjdgur327@gmail.com>
2022-01-04 17:19:22 +0900

    feat : PasswordEncoder 설정

--- src/test/java/com/studyolle/account/AccountControllerTest.java
+++ src/test/java/com/studyolle/account/AccountControllerTest.java
@@ -1,5 +1,6 @@
 package com.studyolle.account;
 
+import com.studyolle.domain.Account;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -65,7 +66,11 @@ class AccountControllerTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/"));
 
+        Account findAccount = accountRepository.findByEmail("dltjdgur327@gmail.com");
+        assertNotNull(findAccount);
+        assertNotEquals(findAccount.getPassword(), "12345678");
         assertTrue(accountRepository.existsByEmail("dltjdgur327@gmail.com"));
+
         then(javaMailSender).should().send(any(SimpleMailMessage.class));
     }
 }
