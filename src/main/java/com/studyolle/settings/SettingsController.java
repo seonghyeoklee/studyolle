0ec49d73ea7f2a7637836ecd4ec7e7d4b869d023
seonghyeoklee <dltjdgur327@gmail.com>
2022-01-19 14:01:18 +0900

    feat : 알림 설정

--- src/main/java/com/studyolle/settings/SettingsController.java
+++ src/main/java/com/studyolle/settings/SettingsController.java
@@ -4,6 +4,7 @@ import com.studyolle.account.AccountService;
 import com.studyolle.account.CurrentUser;
 import com.studyolle.domain.Account;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -20,6 +21,7 @@ import javax.validation.Valid;
 public class SettingsController {
 
     private final AccountService accountService;
+    private final ModelMapper modelMapper;
 
     @InitBinder("passwordForm")
     public void initBinder(WebDataBinder webDataBinder) {
@@ -29,7 +31,7 @@ public class SettingsController {
     @GetMapping("/settings/profile")
     public String updateProfileForm(@CurrentUser Account account, Model model) {
         model.addAttribute(account);
-        model.addAttribute(new Profile(account));
+        model.addAttribute(modelMapper.map(account, Profile.class));
 
         return "settings/profile";
     }
@@ -63,5 +65,24 @@ public class SettingsController {
         accountService.updatePassword(account, passwordForm);
         attributes.addFlashAttribute("message", "패스워드를 수정했습니다.");
         return "redirect:/settings/password";
+    }
+
+    @GetMapping("/settings/notifications")
+    public String notifications(@CurrentUser Account account, Model model) {
+        model.addAttribute(account);
+        model.addAttribute(modelMapper.map(account, Notifications.class));
+        return "settings/notifications";
+    }
+
+    @PostMapping("/settings/notifications")
+    public String notifications(@CurrentUser Account account, @Valid Notifications notifications, Errors errors, Model model, RedirectAttributes attributes) {
+        if (errors.hasErrors()) {
+            model.addAttribute(account);
+            return "settings/notifications";
+        }
+
+        accountService.updateNotifications(account, notifications);
+        attributes.addFlashAttribute("message", "알림을 수정했습니다.");
+        return "redirect:/settings/notifications";
     }
 }
